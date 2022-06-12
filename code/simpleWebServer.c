328836729de69d9e6236f7a81a7a4bff36af5d0e
k1rezaei <k1rezaei78@gmail.com>
2022-06-12 21:27:47 +0430

    big refactor

--- code/simpleWebServer.c
+++ code/simpleWebServer.c
@@ -131,18 +131,20 @@ char* relative_file_address(char *location){
 
 // This function reads the content of a webpage to send it the client.
 
-char* read_webpage(char *webpage_location){
+char *read_webpage(char *webpage_location)
+{
 	FILE *f = fopen(webpage_location, "r");
-	if(f==NULL){
+	if (f == NULL)
+	{
 		return NULL;
 	}
 	fseek(f, 0, SEEK_END);
 	long fsize = ftell(f);
 	fseek(f, 0, SEEK_SET);
 	char *webpage = malloc(fsize + 1);
-	fread (webpage, fsize, 1, f);
+	fread(webpage, fsize, 1, f);
 	fclose(f);
-	webpage[fsize]=0;
+	webpage[fsize] = 0;
 	return webpage;
 }
 
@@ -150,62 +152,84 @@ char* read_webpage(char *webpage_location){
 // This function reads the config file and initialize variables such as content location, port number, log file, maximum thread/processes, etc.
 // It reads from the /etc/simpleWebServer/server.conf
 
-void read_config_file(){
-	FILE *config_file;
+void read_config_file()
+{
+	FILE * config_file;
 	char buff[INNERBUFFSIZE];
 	config_file = fopen(config_file_address, "r");
-	if (config_file==NULL){
+	if (config_file == NULL)
+	{
 		printf("couldn't open config file\n");
 		exit(1);
 	}
-	while(1){	
+	while (1)
+	{
 		memset(buff, 0, INNERBUFFSIZE);
-		if(fgets(buff, 255, (FILE*)config_file)==0){
+		if (fgets(buff, 255, (FILE*) config_file) == 0)
+		{
 			break;
-		}if(buff[0]=='#' || buff[0]==' ' || buff[0]=='\n'){
+		}
+		if (buff[0] == '#' || buff[0] == ' ' || buff[0] == '\n')
+		{
 			continue;
 		}
 		char line[2][INNERBUFFSIZE];
-		int start=strstr(buff, " = ")-buff;
-		buff[start]='\0';
+		int start = strstr(buff, " = ") - buff;
+		buff[start] = '\0';
 		strcpy(line[0], buff);
-		strcpy(line[1], buff+start+3);
-		line[1][strlen(line[1])-1]='\0';
-		printf("%s = %s\n", line[0],line[1]);
+		strcpy(line[1], buff + start + 3);
+		line[1][strlen(line[1]) - 1] = '\0';
+		printf("%s = %s\n", line[0], line[1]);
 		/////////////////// 
-		if(strcmp (line[0], "PORTNUM")==0){
+		if (strcmp(line[0], "PORTNUM") == 0)
+		{
 			sscanf(line[1], "%d", &PORTNUM);
-		}else if(strcmp (line[0], "BUFSIZE")==0){
+		}
+		else if (strcmp(line[0], "BUFSIZE") == 0)
+		{
 			sscanf(line[1], "%d", &BUFSIZE);
-		}else if(strcmp (line[0], "MAXPROCORTHREAD")==0){
+		}
+		else if (strcmp(line[0], "MAXPROCORTHREAD") == 0)
+		{
 			sscanf(line[1], "%d", &MAXPROCTHREAD);
-			MAXPROCTHREAD=min(MAXPROCTHREAD, MAXMAXPROCTHREAD);
-		}else if(strcmp (line[0], "WebContentLocation")==0){
+			MAXPROCTHREAD = min(MAXPROCTHREAD, MAXMAXPROCTHREAD);
+		}
+		else if (strcmp(line[0], "WebContentLocation") == 0)
+		{
 			strcpy(content_location, line[1]);
-		}else if(strcmp (line[0], "StartingPageName")==0){
+		}
+		else if (strcmp(line[0], "StartingPageName") == 0)
+		{
 			strcpy(starting_page, line[1]);
-		}else if (strcmp (line[0], "LogFile")==0){
+		}
+		else if (strcmp(line[0], "LogFile") == 0)
+		{
 			strcpy(LogFile, line[1]);
-		}else if (strcmp (line[0], "ErrorHTML")==0){
+		}
+		else if (strcmp(line[0], "ErrorHTML") == 0)
+		{
 			strcpy(Error, line[1]);
-		}else{
+		}
+		else
+		{
 			printf(" Couldn't find %s as a server parameter\n", line[0]);
 		}
 		//////////////////
 	}
-	fclose (config_file);
+	fclose(config_file);
 }
 
 
 // This function reads the user's information (which is in the message of the client to server).
 
-char* get_user_info(char* buf){
-	char* start=strstr(buf, "User-Agent:")+11;
-	char* end=strstr(start, "\n");
-	int length=(end-start);
-	char *user_info=malloc(length+1);
-	memcpy(user_info, start, end-start);
-	user_info[length]='\0';
+char *get_user_info(char *buf)
+{
+	char *start = strstr(buf, "User-Agent:") + 11;
+	char *end = strstr(start, "\n");
+	int length = (end - start);
+	char *user_info = malloc(length + 1);
+	memcpy(user_info, start, end - start);
+	user_info[length] = '\0';
 	return user_info;
 }
 
@@ -214,12 +238,13 @@ char* get_user_info(char* buf){
 // It closes the connection (i.e. socket).
 // It asks the thread/process to send its log to the main thread/ process.
 
-void handle_request_finished(int fd_ind){
-	busy[fd_ind]=0;
+void handle_request_finished(int fd_ind)
+{
+	busy[fd_ind] = 0;
 	closesocket(fd[fd_ind]);
 	#ifdef MULTIPROC
 	#ifdef DEBUG
-	printf("%s\n",log_ret[fd_ind]);
+	printf("%s\n", log_ret[fd_ind]);
 	#endif
 	write(all_pipes[fd_ind][P_WRITE], log_ret[fd_ind], strlen(log_ret[fd_ind]) + 1);
 	#endif
@@ -229,37 +254,36 @@ void handle_request_finished(int fd_ind){
 //This function handles the requests coming from the client and sends the appropriate html/ picture upon the request.
 //At the end, it calls handle_request_finished.
 
-void* handle_request(void* fd_ind_point){
+void *handle_request(void *fd_ind_point)
+{
 	int fd_ind;
-	fd_ind=*(int*)fd_ind_point;
-	busy[fd_ind]=1;
-	read_log_flag[fd_ind]=1;
+	fd_ind = *(int*) fd_ind_point;
+	busy[fd_ind] = 1;
+	read_log_flag[fd_ind] = 1;
 	SOCKET fd_client = fd[fd_ind];
-	char *logOfRequest=malloc(LOGSIZE);
-	log_ret[fd_ind]=logOfRequest;
+	char *logOfRequest = malloc(LOGSIZE);
+	log_ret[fd_ind] = logOfRequest;
 
-
-	char *buf=malloc(BUFSIZE);
+	char *buf = malloc(BUFSIZE);
 	int fdimg;
-	
 
 	time_t rawtime;
 	struct tm * timeinfo;
 	time(&rawtime);
-	timeinfo=localtime(&rawtime);
+	timeinfo = localtime(&rawtime);
 
-	int size=0;
-	snprintf(logOfRequest+size, LOGSIZE, "Time = %s", asctime(timeinfo));
-	size=strlen(logOfRequest);
-	if(fd_client == INVALID_SOCKET)
+	int size = 0;
+	snprintf(logOfRequest + size, LOGSIZE, "Time = %s", asctime(timeinfo));
+	size = strlen(logOfRequest);
+	if (fd_client == INVALID_SOCKET)
 	{
-		snprintf(logOfRequest+size, LOGSIZE, "Connection Failed! Can't Connect to Client \n" );
-		size=strlen(logOfRequest);
+		snprintf(logOfRequest + size, LOGSIZE, "Connection Failed! Can't Connect to Client \n");
+		size = strlen(logOfRequest);
 		handle_request_finished(fd_ind);
-		return (void *)0;
+		return (void*) 0;
 	}
-	snprintf(logOfRequest+size, LOGSIZE, "Client Connection Accepted\n" );
-	size=strlen(logOfRequest);
+	snprintf(logOfRequest + size, LOGSIZE, "Client Connection Accepted\n");
+	size = strlen(logOfRequest);
 	memset(buf, 0, BUFSIZE);
 	#ifdef UNIX
 	read(fd_client, buf, BUFSIZE);
@@ -268,102 +292,112 @@ void* handle_request(void* fd_ind_point){
 	recv(fd_client, buf, BUFSIZ, 0);
 	#endif
 	#ifdef DEBUG
-	printf("BUF ******ind= %d  *******\n%s(***************)\n", fd_ind,buf);
+	printf("BUF ******ind= %d  *******\n%s(***************)\n", fd_ind, buf);
 	#endif
 
-	if(strlen(buf)==0){
-		snprintf(logOfRequest+size, LOGSIZE, "Request is empty!\n" );
-		size=strlen(logOfRequest);
+	if (strlen(buf) == 0)
+	{
+		snprintf(logOfRequest + size, LOGSIZE, "Request is empty!\n");
+		size = strlen(logOfRequest);
 		handle_request_finished(fd_ind);
-		return (void *)0;
+		return (void*) 0;
 	}
 
-	char* user_info=get_user_info(buf);
-	snprintf(logOfRequest+size, LOGSIZE, "User = %s\n",  user_info);
-	size=strlen(logOfRequest);
+	char *user_info = get_user_info(buf);
+	snprintf(logOfRequest + size, LOGSIZE, "User = %s\n", user_info);
+	size = strlen(logOfRequest);
 	char location[INNERBUFFSIZE];
 	int start;
 	int end;
-	start=5;
-	end=strchr(buf+5,' ')-buf;
-	memcpy(location, buf+start, end-start);
-	location[end-start]='\0';
-	
-	if(start==end){
+	start = 5;
+	end = strchr(buf + 5, ' ') - buf;
+	memcpy(location, buf + start, end - start);
+	location[end - start] = '\0';
+
+	if (start == end)
+	{
 		strcpy(location, starting_page);
-		start=0;
-		end=strlen(location);
+		start = 0;
+		end = strlen(location);
 	}
 	#ifdef DEBUG
-		printf("location ******** %s, %s\n",location, relative_file_address(location));
+	printf("location ********%s, %s\n", location, relative_file_address(location));
 	#endif
-	
-	snprintf(logOfRequest+size, LOGSIZE, "Request = %s\n", location);
-	size=strlen(logOfRequest);
-	snprintf(logOfRequest+size, LOGSIZE,"try to read file %s\n",location );
-	size=strlen(logOfRequest);
 
-	if(!strncmp(location+(end-start-4),"html", 4)){
-		char* webpage=read_webpage(relative_file_address(location));
-		if (webpage==NULL){
-			snprintf(logOfRequest+size, LOGSIZE,"couldn't find html file\n");
-			size=strlen(logOfRequest);
-			webpage=read_webpage(relative_file_address(Error));
+	snprintf(logOfRequest + size, LOGSIZE, "Request = %s\n", location);
+	size = strlen(logOfRequest);
+	snprintf(logOfRequest + size, LOGSIZE, "try to read file %s\n", location);
+	size = strlen(logOfRequest);
+
+	if (!strncmp(location + (end - start - 4), "html", 4))
+	{
+		char *webpage = read_webpage(relative_file_address(location));
+		if (webpage == NULL)
+		{
+			snprintf(logOfRequest + size, LOGSIZE, "couldn't find html file\n");
+			size = strlen(logOfRequest);
+			webpage = read_webpage(relative_file_address(Error));
 		}
 		int n;
-		n=strlen(webpage);
+		n = strlen(webpage);
 		#ifdef UNIX
-			write(fd_client, webpage, n-1);
+		write(fd_client, webpage, n - 1);
 		#endif
 		#ifdef WINDOWS
-			send(fd_client, webpage, n-1, 0 );
+		send(fd_client, webpage, n - 1, 0);
 		#endif
 
-	}else{
-		char* address=relative_file_address(location);
+	}
+	else
+	{
+		char *address = relative_file_address(location);
 
 		#ifdef UNIX
 		fdimg = open(address, O_RDONLY);
 		#endif
 		#ifdef WINDOWS
- 		HANDLE fdimg;
+		HANDLE fdimg;
 		fdimg = CreateFileA(location, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 		#endif
 
-		if(fdimg==INVALID_HANDLE_VALUE){
-			snprintf(logOfRequest+size, LOGSIZE,"couldn't find/open file\n");
-			size=strlen(logOfRequest);
-		}else{
+		if (fdimg == INVALID_HANDLE_VALUE)
+		{
+			snprintf(logOfRequest + size, LOGSIZE, "couldn't find/open file\n");
+			size = strlen(logOfRequest);
+		}
+		else
+		{
 			#ifdef UNIX
-				sendfile(fd_client, fdimg, NULL, 200000);
+			sendfile(fd_client, fdimg, NULL, 200000);
 			#endif
 			#ifdef WINDOWS
-				DWORD dwNumToSend = 0;
-				DWORD nNumberOfBytesPerSend=0;              
-				TransmitFile(fd_client, fdimg, dwNumToSend, nNumberOfBytesPerSend, NULL, NULL, 0);
+			DWORD dwNumToSend = 0;
+			DWORD nNumberOfBytesPerSend = 0;
+			TransmitFile(fd_client, fdimg, dwNumToSend, nNumberOfBytesPerSend, NULL, NULL, 0);
 			#endif
 		}
 		CloseHandle(fdimg);
 	}
-	snprintf(logOfRequest+size, LOGSIZE,"Client Connection Closed\n" );
-	size=strlen(logOfRequest);
+	snprintf(logOfRequest + size, LOGSIZE, "Client Connection Closed\n");
+	size = strlen(logOfRequest);
 	handle_request_finished(fd_ind);
-	return (void *)0;
+	return (void*) 0;
 }
-
 
 
 //This function reads the log of the ind_th thread/process.
 
-void read_log(int ind){
-	if(read_log_flag[ind]){
-		char *log=log_ret[ind];
+void read_log(int ind)
+{
+	if (read_log_flag[ind])
+	{
+		char *log = log_ret[ind];
 		strcat(all_logs, "********************\n");
 		strcat(all_logs, log);
 		#ifdef DEBUG
-		printf("$*$*$*$*%d$*$*$*$*$*$\n%s$*$*$*$*$*$*$*$*$*$\n",ind, log);
+		printf("$*$*$*$*%d$*$*$*$*$*$\n%s$*$*$*$*$*$*$*$*$*$\n", ind, log);
 		#endif
-		read_log_flag[ind]=0;
+		read_log_flag[ind] = 0;
 	}
 }
 
@@ -374,19 +408,23 @@ char read_buffer[INNERBUFFSIZE];
 //This function is necessary while serving with multi processes.
 //It reads the log buffer of the running processes.
 
-void check_all_processes(){
+void check_all_processes()
+{
 	int i;
-	for(i=0;i<MAXPROCTHREAD;i+=1){
-		if(busy[i]){
+	for (i = 0; i < MAXPROCTHREAD; i += 1)
+	{
+		if (busy[i])
+		{
 			int len = read(all_pipes[i][P_READ], read_buffer, INNERBUFFSIZE);
-			if (len > 0) {
+			if (len > 0)
+			{
 				read_buffer[len] = 0;
-				read_log_flag[i]=1;
-				log_ret[i]=malloc(INNERBUFFSIZE);
+				read_log_flag[i] = 1;
+				log_ret[i] = malloc(INNERBUFFSIZE);
 				strcpy(log_ret[i], read_buffer);
 				memset(read_buffer, 0, sizeof(read_buffer));
 				read_log(i);
-				busy[i]=0;
+				busy[i] = 0;
 			}
 		}
 	}
@@ -402,18 +440,19 @@ void IntHandler(int sig)
 {
 	#ifndef MULTIPROC
 	int i;
-	for(i=0;i<MAXPROCTHREAD;i++){
+	for (i = 0; i < MAXPROCTHREAD; i++)
+	{
 		read_log(i);
 	}
 	#endif
 	#ifdef MULTIPROC
 	check_all_processes();
 	#endif
-	printf("Interupt detected ...\n All Logs\n%s\n",all_logs);
-	FILE *logfile;
-	char* address=relative_file_address(LogFile);
+	printf("Interupt detected ...\n All Logs\n%s\n", all_logs);
+	FILE * logfile;
+	char *address = relative_file_address(LogFile);
 	printf("\n save logs to: %s\n", address);
-	logfile=fopen(address, "a");
+	logfile = fopen(address, "a");
 	fprintf(logfile, "%s", all_logs);
 	closesocket(fd_server);
 	exit(0);
